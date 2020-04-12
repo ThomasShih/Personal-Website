fc2a366dd10be1591e97d73a96344ae9d0d4a6f9
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-12 14:02:01 -0600

    Adding More Projects

--- run.bat
+++ run.bat
@@ -5,4 +5,5 @@ mkdir .\dist\assets
 copy  .\assets\portraitCircle.svg .\dist\assets\portraitCircle.svg
 copy  .\assets\resume.pdf .\dist\assets\resume.pdf
 copy  .\assets\GitHubIcon.svg .\dist\assets\GitHubIcon.svg
+copy  .\assets\graph.svg .\dist\assets\graph.svg
 parcel index.html
