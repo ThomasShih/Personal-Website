037defe2e4e540799e8afa205b5c288574399a5e
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-23 19:47:09 -0600

    Modified Build Scripts

--- build.bat
+++ build.bat
@@ -2,8 +2,6 @@ del /F /Q .\dist\
 
 mkdir .\dist\assets
 
-copy  .\assets\portraitCircle.svg .\dist\assets\portraitCircle.svg
-copy  .\assets\resume.pdf .\dist\assets\resume.pdf
-copy  .\assets\GitHubIcon.svg .\dist\assets\GitHubIcon.svg
-copy  .\assets\graph.svg .\dist\assets\graph.svg
+copy  .\assets .\dist\assets
+
 parcel build index.html
