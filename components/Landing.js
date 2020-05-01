6538531b9b426da680a8aa57fc92ec367fb57a19
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-01 15:12:39 -0600

    Obfuscated Landing page out of index.js
    In anticipation of integrating React-router

---
+++ components/Landing.js
@@ -0,0 +1,33 @@
+import NavbarLinks from "./NavbarLinks"
+import navbarLinks from "../javaScriptAssets/navbarLinks"
+import ProfileImg from "./ProfileImg"
+import {popIn} from "./utilities/pop.js"
+
+
+function Landing(){
+  const navbarComponents = navbarLinks.map(link => <NavbarLinks key={link.id}
+                                                                data= {{
+                                                                        id:link.id,
+                                                                        key:link.name,
+                                                                        href:"html/" + link.href,
+                                                                        name:link.name,
+                                                                      }}/>)
+
+  React.useEffect(() => {
+                          navbarLinks.map(link => {popIn(link.id)})
+                          popIn("landingPotrait")
+                          popIn("name")
+                        },[])
+
+  return(
+          <div className="landing">
+            <div className="name" id="name">Thomas Shih</div>
+            <ProfileImg/>
+            <div className="navLinks">
+                {navbarComponents}
+            </div>
+          </div>
+        )
+}
+
+export default Landing
--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-import NavbarLinks from "./components/NavbarLinks"
-import navbarLinks from "./javaScriptAssets/navbarLinks"
-import ProfileImg from "./components/ProfileImg"
-
-import {popIn} from "./components/utilities/pop.js"
 import checkCDNexist from "./components/utilities/checkCDN"
+import {BrowserRouter as Router} from "react-router-dom"
 
-function Landing(){
-  const navbarComponents = navbarLinks.map(link => <NavbarLinks key={link.id}
-                                                                data= {{
-                                                                        id:link.id,
-                                                                        key:link.name,
-                                                                        href:"html/" + link.href,
-                                                                        name:link.name,
-                                                                      }}/>)
-
-  React.useEffect(() => {
-                          checkCDNexist()
-                          navbarLinks.map(link => {popIn(link.id)})
-                          popIn("landingPotrait")
-                          popIn("name")
-                        },[])
-
+import App from "./components/App.js"
+const Index = () => {
+  checkCDNexist()
   return(
-          <div className="landing">
-            <div className="name" id="name">Thomas Shih</div>
-            <ProfileImg/>
-            <div className="navLinks">
-                {navbarComponents}
-            </div>
-          </div>
-        )
+    <Router>
+      <App/>
+    </Router>
+  )
 }
 
-ReactDOM.render(<Landing/>,document.getElementById("body"))
+ReactDOM.render(<Index/>,document.getElementById("body"))
