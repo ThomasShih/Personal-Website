c65a495f803861df3d3e8e8301604c4a0f14b74d
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-02 13:55:27 -0600

    Added Loading

--- components/Landing.js
+++ components/Landing.js
@@ -2,18 +2,28 @@ import NavbarLinks from "./NavbarLinks"
 import navbarLinks from "../javaScriptAssets/navbarLinks"
 import {popIn} from "./utilities/pop.js"
 
+const Loading = () => <div className="loadingDotsContainer"><h1 className="loadingDots"></h1></div>
+
 function Landing(){
   const navbarComponents = navbarLinks.map(link => <NavbarLinks key={link.id}
                                                                 className={"landing_NavLink"}
                                                                 data= {link}/>)
+  const [loading,setLoading] = React.useState(true)
 
   React.useEffect(() => {
                           navbarLinks.map(link => {popIn(link.id)})
                           popIn("landingPotrait")
                           popIn("name")
-                        },[])
+                        },[loading])
 
   return(
+        <>
+        { loading? 
+          <>
+            <Loading/>
+            <img style={{width:".01px"}} id="landingPotrait" src="./assets/portraitCircle.svg" onLoad={() => setLoading(false)}></img>
+          </>
+          :
           <div className="landing">
             <div className="name" id="name">Thomas Shih</div>
             <div className="portrait">
@@ -23,7 +33,9 @@ function Landing(){
                 {navbarComponents}
             </div>
           </div>
-        )
+        }
+        </>
+      )
 }
 
 export default Landing
