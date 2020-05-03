d9c32b2b1011653448aea37d55bb7d1c24a78406
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-03 15:03:06 -0600

    Fixed flickering on landing page

--- components/landing/Landing.js
+++ components/landing/Landing.js
@@ -1,6 +1,6 @@
 import NavbarLinks from "../common/NavbarLinks"
 import navbarLinks from "../../javaScriptAssets/navbarLinks"
-import {popIn} from "../utilities/pop.js"
+import {usePop} from "../utilities/pop.js"
 
 const Loading = () => <div className="loadingDotsContainer"><h1 className="loadingDots"></h1></div>
 
@@ -8,15 +8,10 @@ function Landing(){
   document.title = "Thomas Shih: Landing"
   const navbarComponents = navbarLinks.map(link => <NavbarLinks key={link.id}
                                                                 className={"landing_NavLink"}
-                                                                data= {link}
-                                                                style={{opacity:0}}/>)
+                                                                data= {link}/>)
   const [loading,setLoading] = React.useState(true)
 
-  React.useEffect(() => {
-                          navbarLinks.map(link => {popIn(link.id)})
-                          popIn("landingPotrait")
-                          popIn("name")
-                        },[loading])
+  React.useEffect(() => {usePop("testtesttest")},[loading])
 
   return(
         <>
@@ -26,10 +21,10 @@ function Landing(){
             <img style={{width:".01px"}} id="landingPotrait" src="./assets/portraitCircle.svg" onLoad={() => setLoading(false)}></img>
           </>
           :
-          <div className="landing">
-            <div className="name" id="name" style={{opacity:0}}>Thomas Shih</div>
+          <div className="landing" id="testtesttest" style={{opacity:0}}>
+            <div className="name" id="name">Thomas Shih</div>
             <div className="portrait">
-              <img id="landingPotrait" src="./assets/portraitCircle.svg" style={{opacity:0}}></img>
+              <img id="landingPotrait" src="./assets/portraitCircle.svg"></img>
             </div>
             <div className="navLinks">
                 {navbarComponents}
