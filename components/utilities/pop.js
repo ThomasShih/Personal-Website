d5f2aebe83480780b3c3ece3b3904b1e5f2e81e7
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-05 10:33:26 -0600

    Cleanup

--- components/utilities/pop.js
+++ components/utilities/pop.js
@@ -1,31 +1,5 @@
 import {CSSTransition} from 'react-transition-group'
 
-function pop(elementID,scale,opacity){
-  if(elementID === "*"){
-    var id = elementID;
-  }else{
-    var id = "#" + elementID;
-  }
-
-  //processing
-  anime({
-    targets: id,
-    opacity: opacity,
-    scale: scale,
-    easing: "spring(1,200,15,0)", //'spring(mass, stiffness, damping, velocity)'
-  });
-};
-
-function popIn(id){pop(id,[0.5,1],[0,1])};
-function popOut(id){pop(id,[1,0],[1,0])};
-
-function usePop(id){
-  //Make sure default opacity is set to 0
-  popIn(id)
-  const cleanUp = () => {popOut(id)}
-  return cleanUp
-}
-
 const PopAnimation = ({ in: inProp,children,className,id,style}) => {
   console.log(inProp)
   return(
@@ -36,4 +10,4 @@ const PopAnimation = ({ in: inProp,children,className,id,style}) => {
   </div>
 )}
 
-export {popIn,popOut,usePop,PopAnimation}
+export {PopAnimation}
