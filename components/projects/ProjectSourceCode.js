693b914210b6865012d0519971b050e8808cf116
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-06 10:45:52 -0600

    Hotfix, swapping variables

--- components/projects/ProjectSourceCode.js
+++ components/projects/ProjectSourceCode.js
@@ -3,12 +3,12 @@ import HoverEffect from "../utilities/hoverEffect"
 const ProjectSourceCode = ({data}) => (
   <div className="status">
     <h3>Status: {data.length}</h3>
-    {data.link!=undefined &&  
+    {data.gitHub!=undefined &&  
       <HoverEffect hoverID={`${data.id}Source`}>
         <a href={data.gitHub}>Source Code</a>
       </HoverEffect>
     }
-    {data.gitHub!=undefined &&  
+    {data.link!=undefined &&  
       <HoverEffect hoverID={`${data.id}Hosted`}>
         <a href={data.link}>Hosted Site</a>
       </HoverEffect>
