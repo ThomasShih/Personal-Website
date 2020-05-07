5e3b22c63232666421638338a9dab3a84b409fae
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-07 13:59:40 -0600

    Made it such that the flair in landing only places on first load

--- components/landing/Flair.js
+++ components/landing/Flair.js
@@ -1,14 +1,24 @@
 import Typist from 'react-typist'
 const flairList = ["Front-End Developer","Python Data Analyst","Calculus Tutor","Photographer"]
+var animationPlayed = false
 
-const flairComponents = flairList.map((flair,index) => <li key={index}>{flair}<Typist.Delay ms={300} /></li>)
-
-const Flair = () => (
-  <Typist startDelay={500} cursor={{show:false}}>
-    <ul>
-      {flairComponents}
-    </ul>
-  </Typist>
-)
+function Flair(){
+  if(animationPlayed){
+    return(
+      <ul>
+        {flairList.map((flair,index) => <li key={index}>{flair}</li>)}
+      </ul>
+    )
+  }else{
+    animationPlayed = true
+    return(
+      <Typist startDelay={500} cursor={{show:false}}>
+        <ul>
+          {flairList.map((flair,index) => <li key={index}>{flair}<Typist.Delay ms={300} /></li>)}
+        </ul>
+      </Typist>
+    )
+  }
+}
 
 export default Flair
