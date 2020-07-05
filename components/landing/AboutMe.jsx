fcb75409109b7652100889e5258ec7c2224cfec5
Thomas Shih <55861476+ThomasShih@users.noreply.github.com>
2020-07-05 13:32:54 -0600

    Update AboutMe.jsx

--- components/landing/AboutMe.jsx
+++ components/landing/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () =>
   <Component>
     Hello! I am a young professional with a passion for math, computer science, and nature. 
     <br/><br/>
-    I am currently looking for front-end development positons. I am also available for freelancing where my skills apply. To contact me, please find my information in the footer below.
+    I am available for freelancing where my skills apply. To contact me, please find my information in the footer below.
   </Component>
 
 export default AboutMe
